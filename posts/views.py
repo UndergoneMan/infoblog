a1a2f7be3d2566c6cdd5598a709e4740d3773ee0
UndergoneMan <pizdodel1337@gmail.com>
2020-01-25 18:42:11 +0400

    Templates for posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from posts.models import Post
+
 
 def post_page(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'posts': Post.objects.all()})
